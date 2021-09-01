8e1ce0739d4c679574c0c934cb63682682673157
DESKTOP-BTO9KQS\leeseungmin <metrog950@gmail.com>
2021-09-01 17:19:48 +0900

    feat: 회원가입 valid 전처리기능 추가

--- Photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
+++ Photogram/src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -8,8 +8,15 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor//final 필드를 DI 할때 사용
 @Controller//1.Ioc 2.파일을 리턴하는 컨트롤러
@@ -20,24 +27,33 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/auth/signin")
-    public String signinForm(){
+    public String signinForm() {
         return "auth/signin";
     }
 
     @GetMapping("/auth/signup")
-    public String signupForm(){
+    public String signupForm() {
         return "auth/signup";
     }
 
     //회원가입버튼->/auth/signup->/auth/signin
     @PostMapping("/auth/signup")
-    public String signup(SignupDto signupDto){//key=value(x-www-form-urlencoded)
-        log.info(signupDto.toString());
-        //User<-SignupDto
-        User user = signupDto.toEntity();
-        log.info(user.toString());
-        User userEntity=authService.회원가입(user);
-        System.out.println(userEntity);
-        return "auth/signin";
+    public @ResponseBody
+    String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {//key=value(x-www-form-urlencoded)
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+                System.out.println(error.getDefaultMessage());
+            }
+            return "오류남";
+        } else {
+            //User<-SignupDto
+            User user = signupDto.toEntity();
+            User userEntity = authService.회원가입(user);
+            System.out.println(userEntity);
+            return "auth/signin";
+        }
     }
 }
--- Photogram/src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ Photogram/src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -3,12 +3,21 @@ package com.cos.photogramstart.web.dto.auth;
 import com.cos.photogramstart.domain.user.User;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data//Getter,Setter
 public class SignupDto {
 
+    @Size(min=2,max=20)
+    @NotBlank//백엔드에서도 막아줘야한다
     private String username;
+    @NotBlank
     private String password;
+    @NotBlank
     private String email;
+    @NotBlank
     private String name;
 
     public User toEntity(){
